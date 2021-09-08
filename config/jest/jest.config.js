3eb4228e263d624462271559fd31652dc9e63cea
Daniel Castro <danicc097@gmail.com>
2021-09-08 17:59:44 +0200

    test

--- config/jest/jest.config.js
+++ config/jest/jest.config.js
@@ -1,5 +1,6 @@
+import '@testing-library/jest-dom';
+
 module.exports = async () => {
-    
     console.log(`Running ${process.cwd()}/jest.config.ts`);
     console.log(`Running ${process.cwd()}/jest.config.ts`);
     console.log(`Running ${process.cwd()}/jest.config.ts`);
@@ -7,21 +8,23 @@ module.exports = async () => {
     //add in what you need to do here
     Object.defineProperty(window, 'matchMedia', {
         writable: true,
-        value: jest.fn().mockImplementation(query => ({
-          matches: false,
-          media: query,
-          onchange: null,
-          addListener: jest.fn(), // deprecated
-          removeListener: jest.fn(), // deprecated
-          addEventListener: jest.fn(),
-          removeEventListener: jest.fn(),
-          dispatchEvent: jest.fn(),
+        value: jest.fn().mockImplementation((query) => ({
+            matches: false,
+            media: query,
+            onchange: null,
+            addListener: jest.fn(), // deprecated
+            removeListener: jest.fn(), // deprecated
+            addEventListener: jest.fn(),
+            removeEventListener: jest.fn(),
+            dispatchEvent: jest.fn(),
         })),
-      });
-      global.matchMedia = global.matchMedia || function () {
-        return {
-          addListener: jest.fn(),
-          removeListener: jest.fn(),
+    });
+    global.matchMedia =
+        global.matchMedia ||
+        function () {
+            return {
+                addListener: jest.fn(),
+                removeListener: jest.fn(),
+            };
         };
-      };
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,101 @@
-import * as React from "react"
+import * as React from 'react';
 import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
-import WithSubnavigation from "./WithSubnavigation"
+    ChakraProvider,
+    Box,
+    Text,
+    Link,
+    VStack,
+    Code,
+    Grid,
+    theme,
+    Flex,
+    Heading,
+    Stack,
+    Tag,
+} from '@chakra-ui/react';
+import { ColorModeSwitcher } from './ColorModeSwitcher';
+import { Logo } from './Logo';
+import WithSubnavigation from './WithSubnavigation';
 
 export const App = () => (
-  <ChakraProvider theme={theme}>
-    <WithSubnavigation />
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
+    <ChakraProvider theme={theme}>
+        <WithSubnavigation />
         <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
-  </ChakraProvider>
-)
-
+        <Box borderRadius="lg" boxShadow="sm" pl={3} pr={3} pt={5} pb={5}>
+            <Flex
+                display="flex"
+                flexDirection="column"
+                alignItems="flex-start"
+                justifyContent="flex-start"
+                mb={2}
+                pl={4}
+            >
+                <Heading size="md" as="h2" lineHeight="shorter" fontWeight="bold" fontFamily="heading">
+                    Editor
+                </Heading>
+            </Flex>
+            <Stack spacing={5} pl={4} pt={4}>
+                <Stack spacing={2}>
+                    <Flex p={0} alignItems="space-between" justifyContent="flex-start" display="flex">
+                        <Tag size="md" variant="subtle" colorScheme="pink" mr={3}>
+                            TEST
+                        </Tag>
+                        <Tag size="md" variant="subtle" colorScheme="pink" mr={3}>
+                            estsetestgse
+                        </Tag>
+                        <Tag size="md" variant="subtle" colorScheme="purple" mr={3}>
+                            VsdFE
+                        </Tag>
+                    </Flex>
+                    <Text color="gray.600">
+                        The Builder mode adds extra padding/border to ease components selection (like containers).
+                    </Text>
+                </Stack>
+                <Stack>
+                    <Tag size="md" variant="solid" colorScheme="facebook">
+                        Code Panel
+                    </Tag>
+                    <Text color="gray.600">
+                        Toggle the code panel for viewing the JSX/React code of your components. You can even export
+                        your code directly to CodeSandbox!
+                    </Text>
+                </Stack>
+                <Stack spacing={4}>
+                    <Tag size="md" variant="solid" colorScheme="facebook">
+                        Shortcuts
+                    </Tag>
+                    <Stack isInline>
+                        <Tag size="sm" variant="subtle">
+                            cmd+z
+                        </Tag>
+                        <Tag size="sm" variant="subtle" colorScheme="gray">
+                            ctrl+z
+                        </Tag>
+                        <Text color="gray.600">Undo</Text>
+                    </Stack>
+                    <Stack isInline>
+                        <Tag size="sm" variant="subtle" colorScheme="gray">
+                            cmd+y
+                        </Tag>
+                        <Tag size="sm" variant="subtle" colorScheme="gray">
+                            ctrl+y
+                        </Tag>
+                        <Text color="gray.600">Redo</Text>
+                    </Stack>
+                    <Stack isInline>
+                        <Tag size="md" variant="subtle" colorScheme="gray">
+                            b
+                        </Tag>
+                        <Text color="gray.600">Toggle Builder mode</Text>
+                    </Stack>
+                    <Stack isInline>
+                        <Tag size="sm" variant="subtle" colorScheme="gray">
+                            c
+                        </Tag>
+                        <Text color="gray.600">Toggle Code panel</Text>
+                    </Stack>
+                </Stack>
+            </Stack>
+        </Box>
+    </ChakraProvider>
+);
